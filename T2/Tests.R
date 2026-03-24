b36e355c88589241a935c68b6ed0abed2d259cf6
Jesus Bosch <jrbayguade@gmail.com>
2026-03-24 15:23:32 +0100

    R tests

---
+++ T2/Tests.R
@@ -0,0 +1,20 @@
+a<-c(2,6,5)
+a
+
+x<-rep(0, 10)
+x
+
+length(x)
+
+genero<-c("Hombre","Mujer")
+genero
+
+matriz<-matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9), 3, 3)
+matriz
+dim(matriz)
+
+matriz+matriz
+
+matriz*matriz
+
+
